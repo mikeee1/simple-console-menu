ac349916e7e9e42c0417d06d9c00b677dfe34b30
mikeee1 <mikevansmaalen@gmail.com>
2025-09-20 15:02:15 +0200

    Refactor menu_old functions to use shared implementations

--- src/simple_console_menu/Menu_old.py
+++ src/simple_console_menu/Menu_old.py
@@ -1,4 +1,4 @@
-import math
+from .Menu import simple_console_menu, simple_console_menu_block
 
 
 def SimpleConsoleMenu(
@@ -25,79 +25,16 @@ def SimpleConsoleMenu(
         acceptedQuitCharacters - Optional  : specifier which character is allowed if onlyReturnNumber is False for quit (str)
 
     """
-    chooseLoop = True
-    menuItemsList = []
-    allowedCharactersList = []
-    if isinstance(menuItems, str):
-        menuItemsList = str(menuItems).split(";")
-    else:
-        menuItemsList = menuItems
-    if allowedCharacters != "":
-        allowedCharactersList = str(allowedCharacters).split(";")
-    if autoAddQuit:
-        menuItemsList.append("Quit")
-    menuNumber = 1
-    chooseAmount = 0
-    menuNameLength = len(menuName) + 2
-    menuNameLength = menuSize - menuNameLength
-    menuNameLength /= 2
-    if (menuNameLength % 2) == 0:
-        # even
-        print(int(menuNameLength) * "-", menuName, int(menuNameLength) * "-")
-    else:
-        # uneven
-        menuNameLength1 = math.ceil(menuNameLength)
-        menuNameLength2 = int(math.floor(menuNameLength))
-        print(int(menuNameLength1) * "-", menuName, int(menuNameLength2) * "-")
-    for x in menuItemsList:
-        print(f"{menuNumber}. {x}")
-        menuNumber += 1
-    print(menuSize * "-")
-    while chooseLoop:
-        choose = input(inputQuestion)
-        if onlyReturnNumber:
-            if choose.isdigit():
-                chooseLoop = False
-        else:
-            if acceptedQuitCharacters != "":
-                if (
-                    (choose != "" and choose in allowedCharactersList)
-                    or choose.isdigit()
-                    or choose == acceptedQuitCharacters
-                ):
-                    chooseLoop = False
-            elif (choose != "" and choose in allowedCharactersList) or choose.isdigit():
-                chooseLoop = False
-
-        if chooseAmount >= 20 and chooseLoop:
-            menuNumber = 1
-            if (menuNameLength % 2) == 0:
-                # even
-                print(int(menuNameLength) * "-", menuName, int(menuNameLength) * "-")
-            else:
-                # uneven
-                menuNameLength1 = math.ceil(menuNameLength)
-                menuNameLength2 = int(math.floor(menuNameLength))
-                print(int(menuNameLength1) * "-", menuName, int(menuNameLength2) * "-")
-            for x in menuItemsList:
-                print(f"{menuNumber}. {x}")
-                menuNumber += 1
-            print(76 * "-")
-            chooseAmount = 1
-        chooseAmount += 1
-    if choose.isdigit():
-        if int(choose) == menuNumber - 1 and autoAddQuit:
-            quit()
-    elif choose == acceptedQuitCharacters:
-        quit()
-    # else:
-    if onlyReturnNumber:
-        return int(choose)
-    else:
-        if choose.isdigit():
-            return int(choose)
-        else:
-            return choose
+    return simple_console_menu(
+        menuName,
+        menuItems,
+        input_question=inputQuestion,
+        menu_size=menuSize,
+        auto_add_quit=autoAddQuit,
+        only_return_number=onlyReturnNumber,
+        allowed_characters=allowedCharacters,
+        accepted_quit_characters=acceptedQuitCharacters,
+    )
 
 
 def SimpleConsoleMenuBlock(
@@ -124,94 +61,13 @@ def SimpleConsoleMenuBlock(
         acceptedQuitCharacters - Optional  : specifier which character is allowed if onlyReturnNumber is False for quit (str)
 
     """
-    chooseLoop = True
-    menuItemsList = []
-    allowedCharactersList = []
-    if isinstance(menuItems, str):
-        menuItemsList = str(menuItems).split(";")
-    else:
-        menuItemsList = menuItems
-    if allowedCharacters != "":
-        allowedCharactersList = str(allowedCharacters).split(";")
-    if autoAddQuit:
-        menuItemsList.append("Quit")
-    menuNumber = 1
-    chooseAmount = 0
-    menuNameLength = len(menuName) + 2
-    menuNameLength = menuSize - menuNameLength
-    menuNameLength /= 2
-    if (menuNameLength % 2) == 0:
-        print(
-            "╭" + int(menuNameLength - 1) * "─",
-            menuName,
-            int(menuNameLength - 1) * "─" + "╮",
-        )
-    else:
-        menuNameLength1 = math.ceil(menuNameLength)
-        menuNameLength2 = int(math.floor(menuNameLength))
-        print(
-            "╭" + int(menuNameLength1 - 1) * "─",
-            menuName,
-            int(menuNameLength2 - 1) * "─" + "╮",
-        )
-    for x in menuItemsList:
-        menuItemsWithNumbers = str(menuNumber) + ". " + x
-        print(
-            f"│{menuItemsWithNumbers}"
-            + ((menuSize - 2) - len(menuItemsWithNumbers)) * " "
-            + "│"
-        )
-        menuNumber += 1
-    print("╰" + (menuSize - 2) * "─" + "╯")
-    while chooseLoop:
-        choose = input(inputQuestion)
-        if onlyReturnNumber:
-            if choose.isdigit():
-                chooseLoop = False
-        else:
-            if acceptedQuitCharacters != "":
-                if (
-                    (choose != "" and choose in allowedCharactersList)
-                    or choose.isdigit()
-                    or choose == acceptedQuitCharacters
-                ):
-                    chooseLoop = False
-            elif (choose != "" and choose in allowedCharactersList) or choose.isdigit():
-                chooseLoop = False
-
-        if chooseAmount >= 20 and chooseLoop:
-            menuNumber = 1
-            if (menuNameLength % 2) == 0:
-                print(int(menuNameLength) * "-", menuName, int(menuNameLength) * "-")
-            else:
-                menuNameLength1 = math.ceil(menuNameLength)
-                menuNameLength2 = int(math.floor(menuNameLength))
-                print(int(menuNameLength1) * "-", menuName, int(menuNameLength2) * "-")
-            for x in menuItemsList:
-                print(f"{menuNumber}. {x}")
-                menuNumber += 1
-            print(76 * "-")
-            chooseAmount = 1
-        chooseAmount += 1
-    if choose.isdigit():
-        if int(choose) == menuNumber - 1 and autoAddQuit:
-            quit()
-    elif choose == acceptedQuitCharacters:
-        quit()
-    if onlyReturnNumber:
-        return int(choose)
-    else:
-        if choose.isdigit():
-            return int(choose)
-        else:
-            return choose
-
-
-# print(SimpleConsoleMenuBlock('menu',["item1","item2","item3"],'Number:',20,True,True))
-
-# ╭─────────────╮
-# │    lorem    │
-# │    ipsum    │
-# │    dolor    │
-# ╰─────────────╯
-# print(SimpleConsoleMenu('menu','item1;item2','Number:',20,True,False,'a;b','c'))
+    return simple_console_menu_block(
+        menuName,
+        menuItems,
+        input_question=inputQuestion,
+        menu_size=menuSize,
+        auto_add_quit=autoAddQuit,
+        only_return_number=onlyReturnNumber,
+        allowed_characters=allowedCharacters,
+        accepted_quit_characters=acceptedQuitCharacters,
+    )
